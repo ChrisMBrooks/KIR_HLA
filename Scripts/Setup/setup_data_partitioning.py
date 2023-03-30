000b82ef492430f6337fcfc91bdb0b467433cbab
ChrisMBrooks <chrismbrooks@gmail.com>
2023-03-30 20:27:40 +0100

    Filtering of Steroid Users to Data Partitioning

--- Scripts/Setup/setup_data_partitioning.py
+++ Scripts/Setup/setup_data_partitioning.py
@@ -28,6 +28,12 @@ df_mapping_tbl = sql.read_table_into_data_frame(schema_name=schema_name,
     table_name=table_names[2]
 )
 
+filename = '/Users/chrismbrooks/Documents/Imperial/Asquith Group/Raw Data/Twins UK/E1139_14092021/E1139_140921.xlsx'
+cofactor_codes_df = pd.read_excel(filename, sheet_name='Coding', header=0)
+
+filename = '/Users/chrismbrooks/Documents/Imperial/Asquith Group/Raw Data/Twins UK/E1139_14092021/E1139_140921.xlsx'
+cofactors_df = pd.read_excel(filename, sheet_name='Phenotype_Cont', header=1)
+
 # Concatenate into a Single Dataframe
 mster_df = df_mapping_tbl.merge(df_hla_geno_tbl, 
     on='public_id', how='inner').merge(df_kir_geno_tbl, 
@@ -44,15 +50,24 @@ mster_df['complete_record'] = complete_record_status
 mster_df = mster_df[mster_df['complete_record'] > 0].copy() 
 mster_df['subject_id'] = mster_df['flow_jo_subject_id'] 
 
-print(mster_df['subject_id'].values)
+#Filter Out Steroid Users.
+
+cofactor_codes_df = pd.read_excel(filename, sheet_name='Coding', header=0)
+cofactor_codes_df = cofactor_codes_df[cofactor_codes_df['PhenotypeID'] == 'P003135'].copy()
+cofactor_codes_df = cofactor_codes_df[cofactor_codes_df['ResponseDescription'] == 'YC'].copy()
+code = float(cofactor_codes_df['ResponseCode'].values[0])
+
+cofactors_df = cofactors_df.drop_duplicates(subset=['PublicID'], keep='first')
+mster_df = mster_df.merge(cofactors_df, how='left', left_on='public_id', right_on='PublicID')
+mster_df = mster_df[mster_df['P003135'] != code].copy() 
 
 # Train-Validation Data Split
-dataset_indeces = set(np.arange(0, mster_df['public_id'].shape[0], 1))
+dataset_indeces = sorted(set(np.arange(0, mster_df['public_id'].shape[0], 1)))
 
 training_size = math.floor(.8*mster_df['public_id'].shape[0])
 training_set = set(random.sample(dataset_indeces, k=training_size))
 
-validation_set = dataset_indeces.difference(training_set)
+validation_set = set(dataset_indeces).difference(training_set)
 
 training_df = pd.DataFrame(mster_df.iloc[list(training_set)][['public_id', 'subject_id']])
 training_df.insert(loc=1, column='validation_partition', value="TRAINING")
@@ -62,6 +77,10 @@ validation_df.insert(loc=1, column='validation_partition', value="VALIDATION")
 
 train_valid_df = training_df.append(validation_df)
 
+print('training df shape: ', training_df.shape)
+print('validation df shape: ', validation_df.shape)
+print('train+valid df shape: ', train_valid_df.shape)
+
 values = [list(x) for x in train_valid_df.values]
 
 # Write to SQL Table
@@ -70,3 +89,4 @@ sql.insert_records(schema_name="KIR_HLA_STUDY",
     column_names=['public_id', 'validation_partition', 'subject_id'], 
     values=values
 )
+print("Complete.")
